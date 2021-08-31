4011c286e217598392b1708f2b008fe9ef61f5ee
seo <westkite1201@gmail.com>
2021-09-01 02:00:42 +0900

    Feat : styled-components light, dark thema import

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,16 +1,31 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import { AppProps } from 'next/app';
-import GlobalStyle from 'styles/global-styles';
-import { wrapper } from 'store';
-import Layout from 'components/base/Layout';
+import React, { useState } from "react";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "styles/global-styles";
+import { FiSun, FiMoon } from "react-icons/fi";
+import { wrapper } from "store";
+import Layout from "components/base/Layout";
+import { Toggler } from "components/common/Toggler";
+
 const app = ({ Component, pageProps }: AppProps) => {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+  const icon = theme === "light" ? <FiMoon size={26} /> : <FiSun size={26} />;
+
   return (
     <>
-      <GlobalStyle />
-      <Layout>
-        <Component {...pageProps} title="" />
-      </Layout>
-      <div id="root-modal" />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <NavBar>
+          <Toggler onClick={() => toggleTheme()}>{icon}</Toggler>
+        </NavBar>
+        <Layout>
+          <Component {...pageProps} title="" />
+        </Layout>
+        <div id="root-modal" />
+      </ThemeProvider>
     </>
   );
 };
--- front/styles/global-styles.ts
+++ front/styles/global-styles.ts
@@ -1,6 +1,5 @@
-import { createGlobalStyle } from 'styled-components';
-import { reset } from 'styled-reset';
-import { media } from './theme';
+import { createGlobalStyle, css } from "styled-components";
+import { reset } from "styled-reset";
 
 export const GlobalStyle = createGlobalStyle`
     ${reset}
@@ -16,39 +15,16 @@ export const GlobalStyle = createGlobalStyle`
         -webkit-text-size-adjust: none;
         font-family: -apple-system,BlinkMacSystemFont,helvetica,Apple SD Gothic Neo,sans-serif;       
         font-display: fallback;
-        ${media.tablet}{
-            font-size: 10px;
-        }
         -ms-overflow-style: none;
         scrollbar-width: none;
     }
-    button {
-        background: none;
-        padding: 0;
-        border: none;
-        cursor: pointer;
-        &:disabled {
-            cursor: default;
-            fill: #f2f3f4;
+    ${({ theme }) => {
+      return css`
+        body {
+          font-family: ${theme.fonts.family.base};
+          font-weight: ${theme.fonts.weight.normal};
+          font-size: ${theme.fonts.size.base};
         }
-    }
-
-    .pc-tablet-only {
-        display: block;
-        ${media.mobile} {
-            display: none;
-        }
-    }
-    .tablet-mobile-only{
-        display: none;
-        ${media.tablet}{
-            display:block;
-        }
-    }
-    .mobile-only {
-        display: none;
-        ${media.mobile} {
-            display: block;
-        }
-    }
+      `;
+    }}
 `;
